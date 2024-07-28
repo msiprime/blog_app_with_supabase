97532f6bde66f91529872c1bbb9e39781c152af5
msiprime <msisakib958@gmail.com>
2024-07-29 01:04:13 +0600

    log out implemented, furnishing, refactor remains

--- lib/features/blog/presentation/pages/blog_page.dart
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -1,6 +1,8 @@
 import 'package:capestone_test/core/common/widget/loader.dart';
 import 'package:capestone_test/core/theme/app_pallete.dart';
 import 'package:capestone_test/core/util/show_snackbar.dart';
+import 'package:capestone_test/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:capestone_test/features/auth/presentation/pages/login_page.dart';
 import 'package:capestone_test/features/blog/presentation/blocs/blog_bloc.dart';
 import 'package:capestone_test/features/blog/presentation/pages/add_blog_page.dart';
 import 'package:capestone_test/features/blog/presentation/widgets/blog_card.dart';
@@ -30,14 +32,61 @@ class _BlogPageState extends State<BlogPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: NavigationDrawer(
+        tilePadding: const EdgeInsets.symmetric(vertical: 10),
+        children: [
+          const UserAccountsDrawerHeader(
+            decoration: BoxDecoration(
+              color: AppPallete.gradient1,
+              gradient: LinearGradient(
+                colors: [
+                  AppPallete.gradient1,
+                  AppPallete.gradient2,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            currentAccountPicture: CircleAvatar(
+              child: Icon(
+                Icons.person,
+              ),
+            ),
+            accountName: Text(
+              'Msi Sakib',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            accountEmail: Text('msisakib958@gmail.com'),
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('Logout'),
+            onTap: () {
+              context.read<AuthBloc>().add(const AuthSignOutEvent());
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const LoginPage(),
+                ),
+                (route) => false,
+              );
+            },
+          ),
+        ],
+      ),
       appBar: AppBar(
         title: const Text('Blog Page'),
         actions: [
           IconButton(
             icon: const Icon(Icons.add_circle_outline),
             onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const AddBlogPage()));
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const AddBlogPage()),
+              );
             },
           ),
         ],
