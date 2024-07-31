f1e584291a43fc3a43d95d9760fe3f4108e2d7b7
msiprime <msisakib958@gmail.com>
2024-07-31 16:19:13 +0600

    logger improved for bloc,
    in future we need to integrate class events to copyWith pattern.
    
    logout still giving us trouble.

--- lib/core/util/logger.dart
+++ lib/core/util/logger.dart
@@ -6,25 +6,23 @@ class Log {
 
   Log._internal() {
     _logger = Logger(
-      printer: PrettyPrinter(
-        methodCount: 2,
-        // number of method calls to be displayed
-        errorMethodCount: 8,
-        // number of method calls if stacktrace is provided
-        lineLength: lineLength,
-        // width of the output
-        colors: true,
-        // Colorful log messages
-        printEmojis: true,
-        // Print an emoji for each log message
-        printTime: false, // Should each log print contain a timestamp
+      printer: PrefixPrinter(
+        PrettyPrinter(
+          methodCount: 2,
+          errorMethodCount: 8,
+          colors: true,
+          lineLength: 120,
+          // colors: io.stdout.supportsAnsiEscapes,
+          printEmojis: true,
+          //dateTimeFormat: DateTimeFormat.onlyTimeAndSinceStart,
+        ),
       ),
     );
   }
 
   static final Log _singleton = Log._internal();
 
-  static void verbose(String message) => _singleton._logger.v(message);
+  static void trace(String message) => _singleton._logger.t(message);
 
   static void debug(String message) => _singleton._logger.d(message);
 
