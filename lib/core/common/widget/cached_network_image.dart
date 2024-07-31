7a62f21104e8d7436bdcaa3413fe4f5da5d5a3f0
msiprime <msisakib958@gmail.com>
2024-07-31 20:14:08 +0600

    tweak animations

--- lib/core/common/widget/cached_network_image.dart
+++ lib/core/common/widget/cached_network_image.dart
@@ -17,11 +17,14 @@ class CachedNetworkImage extends StatelessWidget {
         if (snapshot.connectionState == ConnectionState.waiting) {
           // just return Circular Progress Bar if u want
           return Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.grey.withOpacity(0.1),
+            ),
             height: 200,
-            color: Colors.grey.withOpacity(0.1),
           )
               .animate(
-                onPlay: (controller) => controller.repeat,
+                onPlay: (controller) => controller.repeat(reverse: true),
               )
               .shimmer(duration: const Duration(seconds: 1));
         } else if (snapshot.hasError) {
@@ -34,7 +37,7 @@ class CachedNetworkImage extends StatelessWidget {
               key: ValueKey(imageUrl),
               fit: BoxFit.cover,
             ),
-          );
+          ).animate(delay: 100.ms).scaleXY();
         }
       },
     );
